76db729953a8845218f173030757fe28596c261b
Andrey Sapronov <andrey.sapronov@cern.ch>
2020-11-04 15:08:22 +0300

    more typos

--- examples/generator-example.py
+++ examples/generator-example.py
@@ -4,7 +4,7 @@ from gasopt.data_load import tses_forecast_data_process
 from gasopt.generator_interface import generator_forecast
 
 # Открыть dataframe с историческими данными
-G = tses_forecast_data_process('./test_data/tses_data.xlsx')
+G = tses_forecast_data_process('./test_data/tses-data.xlsx')
 
 # Получить прогноз для ЦЭС с горизонтом в 2 суток и сохранить в CSV-файл "tses_forecast.csv"
 F = generator_forecast(G, horizon=2,  output_csv='tses_forecast.csv')
